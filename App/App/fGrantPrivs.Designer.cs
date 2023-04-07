caaa80c66b4b781b0723e08bc4171133dedd26a3
Lê Minh Quân <marucube35@gmail.com>
2023-04-07 15:26:33 +0700

    feat (GrantPrivs): xong chức năng cấp quyền cho object

--- App/App/fGrantPrivs.Designer.cs
+++ App/App/fGrantPrivs.Designer.cs
@@ -28,12 +28,172 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "fGrantPrivs";
+            checkBtn = new Button();
+            privileges = new DataGridView();
+            granteeTxtBox = new TextBox();
+            selectPrivCheckBox = new CheckBox();
+            insertPrivCheckBox = new CheckBox();
+            updatePrivCheckBox = new CheckBox();
+            deletePrivCheckBox = new CheckBox();
+            withGrantCheckBox = new CheckBox();
+            granteeLabel = new Label();
+            objectLabel = new Label();
+            grantBtn = new Button();
+            objectList = new CheckedListBox();
+            ((System.ComponentModel.ISupportInitialize)privileges).BeginInit();
+            SuspendLayout();
+            // 
+            // checkBtn
+            // 
+            checkBtn.Location = new Point(355, 63);
+            checkBtn.Name = "checkBtn";
+            checkBtn.Size = new Size(110, 30);
+            checkBtn.TabIndex = 3;
+            checkBtn.Text = "Kiểm tra";
+            checkBtn.UseVisualStyleBackColor = true;
+            checkBtn.Click += checkButton_Click;
+            // 
+            // privileges
+            // 
+            privileges.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            privileges.Location = new Point(12, 128);
+            privileges.Name = "privileges";
+            privileges.RowTemplate.Height = 25;
+            privileges.Size = new Size(776, 243);
+            privileges.TabIndex = 4;
+            // 
+            // granteeTxtBox
+            // 
+            granteeTxtBox.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            granteeTxtBox.Location = new Point(355, 28);
+            granteeTxtBox.Name = "granteeTxtBox";
+            granteeTxtBox.Size = new Size(240, 29);
+            granteeTxtBox.TabIndex = 5;
+            // 
+            // selectPrivCheckBox
+            // 
+            selectPrivCheckBox.AutoSize = true;
+            selectPrivCheckBox.Location = new Point(601, 28);
+            selectPrivCheckBox.Name = "selectPrivCheckBox";
+            selectPrivCheckBox.Size = new Size(64, 19);
+            selectPrivCheckBox.TabIndex = 6;
+            selectPrivCheckBox.Text = "SELECT";
+            selectPrivCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // insertPrivCheckBox
+            // 
+            insertPrivCheckBox.AutoSize = true;
+            insertPrivCheckBox.Location = new Point(601, 53);
+            insertPrivCheckBox.Name = "insertPrivCheckBox";
+            insertPrivCheckBox.Size = new Size(62, 19);
+            insertPrivCheckBox.TabIndex = 7;
+            insertPrivCheckBox.Text = "INSERT";
+            insertPrivCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // updatePrivCheckBox
+            // 
+            updatePrivCheckBox.AutoSize = true;
+            updatePrivCheckBox.Location = new Point(671, 53);
+            updatePrivCheckBox.Name = "updatePrivCheckBox";
+            updatePrivCheckBox.Size = new Size(68, 19);
+            updatePrivCheckBox.TabIndex = 8;
+            updatePrivCheckBox.Text = "UPDATE";
+            updatePrivCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // deletePrivCheckBox
+            // 
+            deletePrivCheckBox.AutoSize = true;
+            deletePrivCheckBox.Location = new Point(671, 28);
+            deletePrivCheckBox.Name = "deletePrivCheckBox";
+            deletePrivCheckBox.Size = new Size(64, 19);
+            deletePrivCheckBox.TabIndex = 9;
+            deletePrivCheckBox.Text = "DELETE";
+            deletePrivCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // withGrantCheckBox
+            // 
+            withGrantCheckBox.AutoSize = true;
+            withGrantCheckBox.Location = new Point(601, 74);
+            withGrantCheckBox.Name = "withGrantCheckBox";
+            withGrantCheckBox.Size = new Size(142, 19);
+            withGrantCheckBox.TabIndex = 10;
+            withGrantCheckBox.Text = "WITH GRANT OPTION";
+            withGrantCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // granteeLabel
+            // 
+            granteeLabel.AutoSize = true;
+            granteeLabel.Location = new Point(355, 9);
+            granteeLabel.Name = "granteeLabel";
+            granteeLabel.Size = new Size(106, 15);
+            granteeLabel.TabIndex = 11;
+            granteeLabel.Text = "Nhập tên user/role";
+            // 
+            // objectLabel
+            // 
+            objectLabel.AutoSize = true;
+            objectLabel.Location = new Point(12, 9);
+            objectLabel.Name = "objectLabel";
+            objectLabel.Size = new Size(94, 15);
+            objectLabel.TabIndex = 13;
+            objectLabel.Text = "Chọn table/view";
+            // 
+            // grantBtn
+            // 
+            grantBtn.Location = new Point(485, 63);
+            grantBtn.Name = "grantBtn";
+            grantBtn.Size = new Size(110, 30);
+            grantBtn.TabIndex = 16;
+            grantBtn.Text = "Cấp quyền";
+            grantBtn.UseVisualStyleBackColor = true;
+            grantBtn.Click += grantBtn_Click;
+            // 
+            // objectList
+            // 
+            objectList.CheckOnClick = true;
+            objectList.FormattingEnabled = true;
+            objectList.Location = new Point(12, 28);
+            objectList.Name = "objectList";
+            objectList.Size = new Size(337, 94);
+            objectList.TabIndex = 17;
+            // 
+            // fGrantPrivs
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(objectList);
+            Controls.Add(grantBtn);
+            Controls.Add(objectLabel);
+            Controls.Add(granteeLabel);
+            Controls.Add(withGrantCheckBox);
+            Controls.Add(deletePrivCheckBox);
+            Controls.Add(updatePrivCheckBox);
+            Controls.Add(insertPrivCheckBox);
+            Controls.Add(selectPrivCheckBox);
+            Controls.Add(granteeTxtBox);
+            Controls.Add(privileges);
+            Controls.Add(checkBtn);
+            Name = "fGrantPrivs";
+            Text = "fGrantPrivs";
+            ((System.ComponentModel.ISupportInitialize)privileges).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button checkBtn;
+        private Button grantBtn;
+        private DataGridView privileges;
+        private TextBox granteeTxtBox;
+        private CheckBox selectPrivCheckBox;
+        private CheckBox insertPrivCheckBox;
+        private CheckBox updatePrivCheckBox;
+        private CheckBox deletePrivCheckBox;
+        private CheckBox withGrantCheckBox;
+        private Label granteeLabel;
+        private Label objectLabel;
+        private CheckedListBox objectList;
     }
 }
--- App/App/fGrantPrivs.cs
+++ App/App/fGrantPrivs.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+﻿using System.Data;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace App
 {
@@ -15,6 +8,92 @@ namespace App
         public fGrantPrivs()
         {
             InitializeComponent();
+
+            // Thiết lập dữ liệu ban đầu cho danh sách các bảng/view
+            objectList.Items.AddRange(getObjectNames());
+            objectList.SelectedIndex = 0;
+        }
+
+        private void checkButton_Click(object sender, EventArgs e)
+        {
+            string granteeName = granteeTxtBox.Text;
+            string query = $"SELECT * FROM DBA_TAB_PRIVS WHERE GRANTEE='{granteeName.ToUpper()}'";
+            var queryData = DataProvider.Instance.ExcuteQuery(query);
+            privileges.DataSource = queryData;
+        }
+
+        private string[] getObjectNames()
+        {
+            string query = $"SELECT * FROM USER_TABLES";
+            var queryData = DataProvider.Instance.ExcuteQuery(query);
+
+            string[] objectNames = new string[queryData.Rows.Count];
+
+            for (int i = 0; i < queryData.Rows.Count; i++)
+            {
+                objectNames[i] = queryData.Rows[i]["TABLE_NAME"].ToString();
+            }
+
+            return objectNames;
+        }
+
+        private string buildGrantQuery(string objectName)
+        {
+            // Lấy danh sách các quyền
+            string[] privs = new string[] {
+                selectPrivCheckBox.Checked ? selectPrivCheckBox.Text : "",
+                insertPrivCheckBox.Checked ? insertPrivCheckBox.Text : "",
+                updatePrivCheckBox.Checked ? updatePrivCheckBox.Text : "",
+                deletePrivCheckBox.Checked ? deletePrivCheckBox.Text : "",
+            };
+            string withGrantPriv = withGrantCheckBox.Checked ? "TRUE" : "FALSE";
+
+            // Tạo chuỗi các quyền
+            string privQuery = "";
+            for (int i = 0; i < privs.Length; i++)
+            {
+                string priv = privs[i];
+                if (priv != "")
+                {
+                    privQuery += $"{priv}";
+                    if (i != privs.Length - 1)
+                        privQuery += ",";
+                }
+            }
+
+            // Lấy tên schema hiện tại
+            string schemaQuery = "SELECT USER FROM DUAL";
+            string currentSchema = DataProvider.Instance.ExcuteQuery(schemaQuery).Rows[0]["USER"].ToString();
+
+            // Lấy tên của user/role
+            string grantee = granteeTxtBox.Text.ToUpper();
+
+            // Tạo query để gọi thủ tục
+            string procQuery = $"BEGIN\n\tusp_GrantObjPrivs('{privQuery}', '{currentSchema}', '{objectName}', '{grantee}', {withGrantPriv});\nEND;";
+
+            return procQuery;
+        }
+
+        private void grantBtn_Click(object sender, EventArgs e)
+        {
+            // Lấy tên các bảng/view được chọn
+            var selectedObject = objectList.CheckedItems;
+
+            // Gọi thủ tục cho từng bảng/view
+            foreach (var objectName in selectedObject)
+            {
+                string procQuery = buildGrantQuery(objectName.ToString());
+                MessageBox.Show(procQuery);
+
+                try
+                {
+                    DataProvider.Instance.ExcuteQuery(procQuery);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
         }
     }
 }
