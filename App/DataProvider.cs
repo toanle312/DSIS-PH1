c9f0f197cb41df0869c0c6f984f3bcad9edb4de0
20120386 <tuanlam689@gmail.com>
2023-04-07 08:40:26 +0700

    Complete user list

---
+++ App/DataProvider.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Oracle.ManagedDataAccess.Client;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+using System.Drawing;
+
+namespace App
+{
+    internal class DataProvider
+    {
+        private static DataProvider _instance;
+
+        public static DataProvider Instance
+        {
+            get { if (_instance == null) _instance = new DataProvider(); return _instance; }
+            private set { _instance = value; }
+        }
+
+        private DataProvider() {  }
+
+        private OracleConnection connection;
+
+        public void DisconnectDB()
+        {
+            connection.Close();
+            connection.Dispose();
+        }
+
+        public string ConnectDB(string usr, string pwd)
+        {
+            String conn = @"Data Source=localhost:1521/xe; User ID=" + usr + ";Password=" + pwd + "";
+            conn = string.Format(conn, usr, pwd);
+
+            connection = new OracleConnection();
+            connection.ConnectionString = conn;
+
+            try
+            {
+                connection.Open();
+                connection.Close();
+                return "Success";
+            }
+            catch (OracleException ex)
+            {
+                connection = null;
+                return ex.Message;
+            }
+        }
+
+        public DataTable ExcuteQuery(string query)
+        {
+            DataTable data = new DataTable();
+            
+            connection.Open();
+
+            OracleCommand cmd = new OracleCommand(query, connection);
+
+            OracleDataAdapter adapter = new OracleDataAdapter(cmd);
+
+            adapter.Fill(data);
+
+            connection.Close();
+
+            return data;
+        }
+
+        public int ExcuteNonQuery(string query)
+        {
+            int data = 0;
+
+            connection.Open();
+
+            OracleCommand cmd = new OracleCommand(query, connection);
+
+            data = cmd.ExecuteNonQuery();
+
+            connection.Close();
+
+            return data;
+        }
+
+        public object ExcuteScalar(string query)
+        {
+            object data = 0;
+
+            connection.Open();
+
+            OracleCommand cmd = new OracleCommand(query, connection);
+
+            data = cmd.ExecuteScalar();
+
+            connection.Close();
+
+            return data;
+        }
+    }
+}
---
+++ App/fUsersList.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace App
+{
+    partial class fUsersList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fUsersList));
+            user_list_view = new DataGridView();
+            label1 = new Label();
+            refresh_btn = new Button();
+            ((System.ComponentModel.ISupportInitialize)user_list_view).BeginInit();
+            SuspendLayout();
+            // 
+            // user_list_view
+            // 
+            user_list_view.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            user_list_view.Location = new Point(59, 110);
+            user_list_view.Name = "user_list_view";
+            user_list_view.RowHeadersWidth = 51;
+            user_list_view.RowTemplate.Height = 29;
+            user_list_view.Size = new Size(832, 337);
+            user_list_view.TabIndex = 4;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = SystemColors.ButtonFace;
+            label1.Font = new Font("Segoe Script", 25.2F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = Color.Coral;
+            label1.Location = new Point(160, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(633, 70);
+            label1.TabIndex = 5;
+            label1.Text = "LIST OF USERS IN SYSTEM";
+            // 
+            // refresh_btn
+            // 
+            refresh_btn.BackColor = SystemColors.ButtonFace;
+            refresh_btn.Cursor = Cursors.Hand;
+            refresh_btn.Image = (Image)resources.GetObject("refresh_btn.Image");
+            refresh_btn.ImageAlign = ContentAlignment.MiddleLeft;
+            refresh_btn.Location = new Point(60, 68);
+            refresh_btn.Name = "refresh_btn";
+            refresh_btn.Size = new Size(94, 36);
+            refresh_btn.TabIndex = 6;
+            refresh_btn.Text = "Refresh";
+            refresh_btn.TextAlign = ContentAlignment.MiddleRight;
+            refresh_btn.UseVisualStyleBackColor = false;
+            refresh_btn.Click += refresh_btn_Click;
+            // 
+            // fUsersList
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(950, 500);
+            Controls.Add(refresh_btn);
+            Controls.Add(label1);
+            Controls.Add(user_list_view);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "fUsersList";
+            Text = "fUsersList";
+            ((System.ComponentModel.ISupportInitialize)user_list_view).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button view_btn;
+        private DataGridView user_list_view;
+        private Label label1;
+        private Button refresh_btn;
+    }
+}
