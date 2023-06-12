8aad3b4c39ebca9883d8cf62616aa98df1316e7c
Nhut Truong <lamnhuttruong2@gmail.com>
2023-06-12 13:17:24 +0700

    Thêm chức năng xem bảng audit

---
+++ PH1/PH1/fAudit.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace PH1
+{
+    partial class fAudit
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            comboBox1 = new ComboBox();
+            label1 = new Label();
+            dataGridView1 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Phân công", "Nhân viên" });
+            comboBox1.Location = new Point(190, 20);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(101, 28);
+            comboBox1.TabIndex = 0;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(39, 23);
+            label1.Name = "label1";
+            label1.Size = new Size(145, 20);
+            label1.TabIndex = 1;
+            label1.Text = "Xem audit trên bảng";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.BackgroundColor = SystemColors.ButtonFace;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 67);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.RowTemplate.Height = 29;
+            dataGridView1.Size = new Size(930, 417);
+            dataGridView1.TabIndex = 2;
+            // 
+            // fAudit
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(954, 496);
+            Controls.Add(dataGridView1);
+            Controls.Add(label1);
+            Controls.Add(comboBox1);
+            Name = "fAudit";
+            Text = "fAudit";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ComboBox comboBox1;
+        private Label label1;
+        private DataGridView dataGridView1;
+    }
+}
---
+++ PH1/PH1/fAudit.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PH1
+{
+    public partial class fAudit : Form
+    {
+        public fAudit()
+        {
+            InitializeComponent();
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCellsExceptHeaders;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // bang phan cong
+            if (comboBox1.SelectedIndex == 0)
+            {
+                string query = "SELECT SESSION_ID, TIMESTAMP, DB_USER, OS_USER, USERHOST, OBJECT_SCHEMA, OBJECT_NAME, POLICY_NAME, SQL_TEXT, STATEMENT_TYPE, EXTENDED_TIMESTAMP FROM DBA_FGA_AUDIT_TRAIL WHERE OBJECT_NAME='PHANCONG$'";
+                dataGridView1.DataSource = null;
+                dataGridView1.DataSource = DataProvider.Instance.ExcuteQuery(query);
+            }
+            // bang nhan vien
+            else
+            {
+                string query = "SELECT SESSION_ID, TIMESTAMP, DB_USER, OS_USER, USERHOST, OBJECT_SCHEMA, OBJECT_NAME, POLICY_NAME, SQL_TEXT, STATEMENT_TYPE, EXTENDED_TIMESTAMP FROM DBA_FGA_AUDIT_TRAIL WHERE OBJECT_NAME='NHANVIEN$'";
+                dataGridView1.DataSource = null;
+                dataGridView1.DataSource = DataProvider.Instance.ExcuteQuery(query);
+            }
+        }
+    }
+}
